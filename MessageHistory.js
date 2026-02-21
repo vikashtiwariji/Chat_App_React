ccaebf1b8e20c20f701a09ea84ca350278004f15
vikashtiwariji <v.d.kumar.tiwari@accenture.com>
2026-02-22 00:28:42 +0530

    Create MessageHistory.js

---
+++ MessageHistory.js
@@ -0,0 +1,21 @@
+import PropTypes from "prop-types";
+
+const MessageHistory = ({messages, user})=>{
+    return(
+        <ul className="message-list">
+            {messages.map((message,index)=>(
+                <li key={index} className={message.username===user.username?"message sender":"message recipient"}>
+                    <p>{`${message.username}:${message.text}`}</p>
+                </li>
+            ))}
+            
+        </ul>
+    );
+};
+
+MessageHistory.propTypes={
+    messages:PropTypes.array.isRequired,
+    user:PropTypes.object.isRequired,
+};
+
+export default MessageHistory;
